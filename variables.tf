b11396de41c4aedb36ffa569bb5aa0bc419dfc90
Beau Labs <46016124+beaulabs@users.noreply.github.com>
2019-06-21 01:07:41 -0600

    modified variables.tf

--- variables.tf
+++ variables.tf
@@ -15,11 +15,6 @@ variable "avm_name" {
   default     = "sandbox"
 }
 
-variable "public_ip_dns" {
-  description = "Optional globally unique per datacenter region domain name label to apply to each public ip address. e.g. thisvar.varlocation.cloudapp.azure.com where you specify only thisvar here. This is an array of names which will pair up sequentially to the number of public ips defined in var.nb_public_ip. One name or empty string is required for every public ip. If no public ip is desired, then set this to an array with a single empty string."
-  default     = [""]
-}
-
 variable "admin_name" {
   description = "The admin name you want to use to access vm if not using ssh key"
   default     = "beau"
